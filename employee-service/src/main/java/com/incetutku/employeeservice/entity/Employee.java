8e0fdcc45f351bdf9b7f5a895ef69b44c91d0a16
Tutku Ince <tutku.ince@outlook.com>
2024-12-09 16:32:48 +0300

    Validation Dependencies and Validation Annotations on Employee Entity

--- employee-service/src/main/java/com/incetutku/employeeservice/entity/Employee.java
+++ employee-service/src/main/java/com/incetutku/employeeservice/entity/Employee.java
@@ -1,11 +1,15 @@
 package com.incetutku.employeeservice.entity;
 
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
 
 @Getter
 @Setter
@@ -16,9 +20,23 @@ public class Employee {
 
     @Id
     private String id;
+
+    @NotBlank
     private String name;
+
     private String surname;
+
+    @Size(min = 10, max = 50)
+    @Email
+    @Pattern(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\\\.[a-z]{2,3}", flags = Pattern.Flag.CASE_INSENSITIVE)
     private String email;
+
     private String position;
 
+    @Min(18)
+    @Max(100)
+    private int age;
+
+    @CreationTimestamp
+    private Date createdDate;
 }
