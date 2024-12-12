01a788f591ed090ff2cc6d516b6f831604e17fca
Tutku Ince <tutku.ince@outlook.com>
2024-12-12 18:25:47 +0300

    update save method

--- task-service/src/main/java/com/incetutku/taskservice/service/TaskServiceImpl.java
+++ task-service/src/main/java/com/incetutku/taskservice/service/TaskServiceImpl.java
@@ -1,6 +1,8 @@
 package com.incetutku.taskservice.service;
 
+import com.incetutku.taskservice.dto.EmployeeDTO;
 import com.incetutku.taskservice.dto.TaskDTO;
+import com.incetutku.taskservice.dto.TaskDetailDTO;
 import com.incetutku.taskservice.entity.PriorityType;
 import com.incetutku.taskservice.entity.Status;
 import com.incetutku.taskservice.entity.Task;
@@ -9,21 +11,51 @@ import com.incetutku.taskservice.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
+    private final TaskDetailService taskDetailService;
+    private final RestTemplate restTemplate;
 
     @Override
     public TaskDTO save(TaskDTO taskDTO) {
-        Task savableTask = TaskMapper.mapToTask(taskDTO);
-        Task savedTask = taskRepository.save(savableTask);
-        return TaskMapper.mapToTaskDTO(savedTask);
+        ResponseEntity<EmployeeDTO> employeeDTOResponseEntity = restTemplate
+                .getForEntity("http://localhost:7202/api/v1/employees/" + taskDTO.getAssignee(), EmployeeDTO.class);
+
+        EmployeeDTO employeeDTO = employeeDTOResponseEntity.getBody();
+
+        if (!Objects.isNull(employeeDTO) && employeeDTOResponseEntity.getStatusCode().is2xxSuccessful()) {
+            Task savableTask = TaskMapper.mapToTask(taskDTO);
+            Task savedTask = taskRepository.save(savableTask);
+
+            taskDTO.setAssignee(employeeDTO.getId());
+            taskDTO.setId(savedTask.getId());
+
+            TaskDetailDTO taskDetailDTO = TaskDetailDTO.builder()
+                    .employeeId(employeeDTO.getId())
+                    .employeeName(employeeDTO.getName())
+                    .employeeSurname(employeeDTO.getSurname())
+                    .taskTitle(taskDTO.getTitle())
+                    .taskDescription(taskDTO.getDescription())
+                    .status(taskDTO.getStatus())
+                    .priority(taskDTO.getPriorityType())
+                    .build();
+
+            taskDetailService.save(taskDetailDTO);
+
+            return TaskMapper.mapToTaskDTO(savedTask);
+        }
+
+        return new TaskDTO();
     }
 
     @Override
