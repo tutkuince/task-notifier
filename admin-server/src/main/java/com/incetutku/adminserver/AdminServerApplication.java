4c7e03c457bc2465d346921032a71ff4674a304f
Tutku Ince <tutku.ince@outlook.com>
2024-12-18 18:24:44 +0300

    Enable Eureka Discovery Client and Admin Server

--- admin-server/src/main/java/com/incetutku/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/com/incetutku/adminserver/AdminServerApplication.java
@@ -1,9 +1,13 @@
 package com.incetutku.adminserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
 public class AdminServerApplication {
 
     public static void main(String[] args) {
